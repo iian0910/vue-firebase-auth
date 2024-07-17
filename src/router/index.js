a1f53475784e1f5fffe88713bb1bb4e6e5db51da
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2024-07-17 17:37:28 +0800

    調整 history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import {
   createRouter,
-  createWebHistory
+  createWebHashHistory
 } from 'vue-router'
 
 const routes = [
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
